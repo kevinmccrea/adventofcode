bbe7581c52fee9d6c89a108f2f399af3de469e5c
Kevin McCrea <km.span@gmail.com>
2015-12-07 22:02:03 -0500

    Completed puzzle day 2

---
+++ 02/puzzle_two.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+f = open('input', 'r')
+
+total_sqft = 0
+total_ribbon = 0
+for line in f.readlines():
+    if line:
+        (l, w, h) = map(int, line.split('x'))
+        (side1, side2, side3) = (2*l*w, 2*w*h, 2*h*l)
+        slack = min((side1, side2, side3))/2
+
+        total_sqft += side1 + side2 + side3 + slack
+
+        ribbon = min((2*l + 2*w, 2*w + 2*h, 2*h + 2*l))
+        bow = l*w*h
+
+        total_ribbon += ribbon + bow
+
+f.close()
+
+print 'Total SQFT: %d' % total_sqft
+print 'Total ribbon: %d' % total_ribbon
+
