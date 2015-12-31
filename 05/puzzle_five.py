6f46d80913513b01ec45b31749244be91b76acd9
Kevin McCrea <kevin.mccrea@axiosengineering.com>
2015-12-31 01:36:24 -0500

    Completed puzzle for day 5

---
+++ 05/puzzle_five.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import re
+
+f = open('input', 'r')
+data = f.readlines()
+f.close()
+
+num_nice = 0
+for line in data:
+    naughty = False
+    if not len(re.findall(r'[aeiou]', line)) >= 3:
+        naughty = True
+
+    if not re.findall(r'(.)\1', line):
+        naughty = True
+
+    if re.findall(r'ab|cd|pq|xy', line):
+        naughty = True
+
+    if not naughty:
+        num_nice += 1
+
+print 'Num nice: %d' % num_nice
+
+num_nice = 0
+for line in data:
+    naughty = False
+    if not re.findall(r'(..).*\1', line):
+        naughty = True
+
+    if not re.findall(r'(.).\1', line):
+        naughty = True
+
+    if not naughty:
+        num_nice += 1
+
+print 'Num nice: %d' % num_nice
