87f5705e6399e224386c11e02aa022151550e58d
Kevin McCrea <km.spam@gmail.com>
2017-12-18 23:03:24 -0500

    updated template

--- template.py
+++ template.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
-f = open('input', 'r')
-data = f.readline().strip()
+import sys
+
+infilename = 'input.txt'
+if len(sys.argv) > 2 and sys.argv[1] == '-i':
+    infilename = sys.argv[2]
+
+print 'Using input file: %s' % infilename
+
+f = open(infilename, 'r')
+data = f.readlines()
 f.close()
 
 
