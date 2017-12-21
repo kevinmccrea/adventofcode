4e311bf70e52fee4e9452adfe570135f87c4f4a7
Kevin McCrea <kevin.mccrea@axiosengineering.com>
2017-12-21 17:19:26 -0500

    Added day 21 input and template

---
+++ 2017/21/puzzle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+
+infilename = 'input.txt'
+if len(sys.argv) > 2 and sys.argv[1] == '-i':
+    infilename = sys.argv[2]
+
+print 'Using input file: %s' % infilename
+
+f = open(infilename, 'r')
+data = f.readlines()
+f.close()
+
+
