9a3597bb4ddfca65d77dd43ac8dea535abb96a50
Kevin McCrea <km.spam@gmail.com>
2019-02-12 22:10:18 -0500

    Added day 20

--- 2018/20/puzzle.py
+++ 2018/20/puzzle.py
@@ -3,6 +3,7 @@
 import sys
 import collections
 import itertools
+import aoc
 
 infilename = 'input.txt'
 if len(sys.argv) > 2 and sys.argv[1] == '-i':
@@ -18,10 +19,29 @@ data = [line.strip() for line in data]
 
 data = data[0]
 
-def expand(chars):
-    branches = []
-    dends = []
+DIRS = { 'N': (-1,0), 'W': (0,-1), 'E': (0,1), 'S': (1, 0)}
 
-    return (branches, dends)
+maze = { (0,0):0 }
 
+stack = []
+pos = (0,0)
+dist = 0
+for cc in data[1:-1]:
+    if cc == '(':
+        stack.append(pos)
+    elif cc == '|':
+        pos = stack[-1]
+        dist = maze[pos]
+    elif cc == ')':
+        pos = stack.pop(-1)
+        dist = maze[pos]
+    else:
+        pos = aoc.tup_add(pos, DIRS[cc])
+        dist += 1
 
+        if not maze.has_key(pos):
+            maze[pos] = dist
+
+print 'Part 1: ', max(maze.values())
+
+print 'Part 2:', len([dd for dd in maze.values() if dd >= 1000])
